feb26205c30342e8cf6126fbd7577b76c13d0c08
shreeshastha <shreeshashre@gmail.com>
2023-04-11 20:37:05 +0545

    validation and create form validation

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,5 +1,8 @@
 <script setup>
+    import axios from 'axios';
     import {ref, onMounted, reactive} from "vue";
+    import {Form, Field} from 'vee-validate';
+    import * as yup from 'yup';
 
     // const users = [
     //     {
@@ -15,6 +18,9 @@
     // ];
 
     const users = ref([]);
+    const editing = ref(false);
+    const formValues = ref();
+    const form = ref(null);
 
     const getUsers = () => {
         axios.get('/api/users')
@@ -23,22 +29,87 @@
             })
     }
 
-    const form = reactive({
-        name: '',
-        email: '',
-        password: '',
+    const createUser = (values, {resetForm, setErrors}) => {
+        axios.post('/api/users', values)
+            .then((response) => {
+            users.value.unshift(response.data);
+            $('#userFormModal').modal('hide');
+            resetForm();
+        }).catch((error) => {
+            if(error.response.data.errors){
+                setErrors(error.response.data.errors);
+            }
+            // setFieldError('email', error.response.data.errors.email[0]);
+        });
+    };
+
+    // const form = reactive({
+    //     name: '',
+    //     email: '',
+    //     password: '',
+    // });
+
+    // const createUser = () => {
+    //     axios.post('/api/users', form)
+    //         .then((response) => {
+    //         users.value.unshift(response.data)
+    //         form.name= '';
+    //         form.email = '';
+    //         form.password = '';
+    //         $('#userFormModal').modal('hide');
+    //     });
+    // }
+
+    const addUser = () => {
+        editing.value = false;
+        $('#userFormModal').modal('show');
+    };
+
+    const editUser = (user) => {
+        editing.value = true;
+        form.value.resetForm();
+        $('#userFormModal').modal('show');
+        formValues.value = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        };
+    };
+
+    const updateUser = (values) => {
+        axios.put('/api/users/' + formValues.value.id, values)
+            .then((response) => {
+                const index = users.value.findIndex(user => user.id === response.data.id);
+                users.value[index] = response.data;
+                $('#userFormModal').modal('hide');
+            }).catch((error) => {
+                console.log(error);
+            }).finally(() => {
+                form.value.resetForm();
+            });
+    };
+
+    const createUserSchema = yup.object({
+        name: yup.string().required(),
+        email: yup.string().email().required(),
+        password: yup.string().required().min(8),
     });
 
-    const createUser = () => {
-        axios.post('/api/users', form)
-        .then((response) => {
-            users.value.unshift(response.data)
-            form.name= '';
-            form.email = '';
-            form.password = '';
-            $('#createUserModal').modal('hide');
-        });
-    }
+    const editUserSchema = yup.object({
+        name: yup.string().required(),
+        email: yup.string().email().required(),
+        password: yup.string().when((password, schema) => {
+            return password != '' ? schema.required().min(8) : schema;
+        })
+    });
+
+    const handelSubmit = (values, actions) => {
+        if(editing.value){
+            updateUser(values, actions);
+        }else{
+            createUser(values, actions);
+        }
+    };
 
     onMounted(() => {
         getUsers();
@@ -65,7 +136,7 @@
 
     <div class="content">
         <div class="container-fluid">
-            <button type="button" class="mb-2 btn btn-primary" data-toggle="modal" data-target="#createUserModal">
+            <button @click="addUser" type="button" class="mb-2 btn btn-primary">
                 Add User
             </button>
             <div class="card">
@@ -89,7 +160,9 @@
                                 <td>{{ user.email }}</td>
                                 <td>-</td>
                                 <td>-</td>
-                                <td>-</td>
+                                <td>
+                                    <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
@@ -98,37 +171,43 @@
         </div>
     </div>
 
-    <div class="modal fade" id="createUserModal" tabindex="-1" role="dialog" aria-labelledby="createUserModalLabel" aria-hidden="true">
+    <div class="modal fade" id="userFormModal" tabindex="-1" role="dialog" aria-labelledby="userFormModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="createUserModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="userFormModalLabel">
+                        <span v-if="editing">Edit User</span>
+                        <span v-else>Add New User</span>
+                    </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    <form action="" autocomplete="off">
+                <Form ref="form" @submit="handelSubmit" :validation-schema="editing ? editUserSchema : createUserSchema" v-slot="{ errors }" :initial-values="formValues">
+                    <div class="modal-body">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input v-model="form.name" type="text" class="form-control" id="name" placeholder="Enter Full Name">
+                            <Field name="name" type="text" class="form-control" :class="{'is-invalid':errors.name}" id="name" placeholder="Enter Full Name"/>
+                            <span class="invalid-feedback">{{ errors.name }}</span>
                         </div>
 
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input v-model="form.email" type="email" class="form-control" id="email" placeholder="Enter Email">
+                            <Field name="email" type="email" class="form-control" :class="{'is-invalid':errors.email}" id="email" placeholder="Enter Email"/>
+                            <span class="invalid-feedback">{{ errors.email }}</span>
                         </div>
 
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input v-model="form.password" type="password" class="form-control" id="password" placeholder="Enter Password">
+                            <Field name="password" type="password" class="form-control" :class="{'is-invalid':errors.password}" id="password" placeholder="Enter Password"/>
+                            <span class="invalid-feedback">{{ errors.password }}</span>
                         </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button @click="createUser()" type="button" class="btn btn-primary">Save changes</button>
-                </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </Form>
             </div>
         </div>
     </div>
